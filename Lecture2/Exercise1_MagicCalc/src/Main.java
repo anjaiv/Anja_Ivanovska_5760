3e405a2947d28870a99271c44a4ba17f67b802ad
Petar Ivanovski <ivanvoskipetar@gmail.com>
2025-10-03 19:35:39 +0200

    10/3/2025

---
+++ Lecture2/Exercise1_MagicCalc/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 5;
+
+        int addition = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double division = (double) a / b;
+
+        System.out.println(" Sum of numbers is:" + addition);
+        System.out.println(" Difference of numbers is" + difference);
+        System.out.println("Product; " + product);
+        System.out.println("Division: " + division);
+
+    }
+}
---
+++ ex_4/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+
+
+    Scanner scanner = new System.in);
+
+while (true) {
+        System.out.println("Enter some year or STOP to exit"
+        String input = scanner.nextLine();
+
+    if (input.equalsIgnoreCase(anotherString: "STOP")){
+        }
+        }
+public static boolean isLeap (int year (
+    if (year % 4!=
