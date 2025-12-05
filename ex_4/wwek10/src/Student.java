4455ac6ddb6690e2f79120f36f41cbe16fcda20c
Anja <anja.ivanovska.04@gmail.com>
2025-12-05 18:49:14 +0100

    week11

---
+++ ex_4/wwek10/src/Student.java
@@ -0,0 +1,12 @@
+ public class Student {
+    String name;
+    int indexNo;
+    int age;
+
+    public Student(String name, int indexNo, int age) {
+        this.name = name;
+        this.indexNo = indexNo;
+        this.age = age;
+    }
+}{
+}
